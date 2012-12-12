dd7d68cf7b8ce9f2a11ed98965b21c3d513698cc
eliotliu <youbinliu@126.com>
2012-12-12 04:27:28 -0500

    commit

--- config/routes.js
+++ config/routes.js
@@ -6,13 +6,14 @@ var _v = 1
 module.exports.setup = function (app, passport) {
   // user routes
   var users = require('../app/controllers/users')
-  app.post('/'+_v+'/users/login', users.login)
-  app.get('/'+_v+'/users/logout', users.logout)
-  app.post('/'+_v+'/users/register', users.register) 
-  app.get('/'+_v+'/users/test', users.test)
-  app.post('/'+_v+'/users/avatar',users.uploadAvatar)
-  app.get('/'+_v+'/users/avatar/:aid',users.avatar)
-  app.post('/'+_v+'/users/modifypwd',users.modifyPwd)
+  var users_pre = '/'+_v+'/users/'
+  app.post(users_pre+'login', users.login)
+  app.get(users_pre+'logout', users.logout)
+  app.post(users_pre+'register', users.register) 
+  app.get(users_pre+'test', users.test)
+  app.post(users_pre+'avatar',users.uploadAvatar)
+  app.get(users_pre+'avatar/:aid',users.avatar)
+  app.post(users_pre+'modifypwd',users.modifyPwd)
   
   app.param('userId', function (req, res, next, id) {
     User
@@ -25,6 +26,14 @@ module.exports.setup = function (app, passport) {
       })
   })
   
- 
-
+  var voice = require("../app/controllers/voices");
+  var voice_pre = '/'+_v+'voices/'
+  app.post(voice_pre+'new',voice.add)
+  app.get(voice_pre+'delete/:vid',voice.delete)
+  app.get(voice_pre+'list/:uid/:pid',voice.list)
+  app.post(voice_pre+'doreply/:vid/'.voice.doreply)
+  app.get(voice_pre+'reply/:vid/:pid',voice.reply)
+  app.get(voice_pre+'show/:vid',voice.show)
+  
+  
 }
