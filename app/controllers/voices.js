345b6c7548ac2f70674065af729009b769c0d669
eliotliu <youbinliu@126.com>
2012-12-12 04:29:38 -0500

    commit

---
+++ app/controllers/voices.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose")
+
+
+exports.add = function(req,res){
+    
+}
+
+exports.delete = function(req,res){
+  
+}
+
+exports.list = function(req,res){
+    
+}
+
+exports.doreply = function(req,res){
+}
+
+exports.reply = function(req,res){
+    
+}
+
+exports.show = function(req,res){    
+    
+}
---
+++ app/models/voice.js
@@ -0,0 +1,6 @@
+// voice schema
+
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema
+
+mongoose.model('User', UserSchema)
