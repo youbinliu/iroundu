e6e201ab8b4d6e9a8a6fa7ddda276d8c2bd90c15
eliotliu <youbinliu@126.com>
2012-12-17 03:01:22 -0500

    commit

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,101 +3,101 @@ var mongoose = require("mongoose")
 ,   Follow = mongoose.model("Follow")
 ,   fs = require("fs")
 ,   util = require("../../lib/util")
-,   FileUpload = require("../../lib/FileUpload").FileUpload
+,   FileUpload = require("../../lib/FileUpload").FileUpload;
 
-exports.authCallback = function(req,res,next){}
+exports.authCallback = function(req,res,next){};
 
 exports.test = function(req,res){
-    res.render('users/test')
-}
+    res.render('users/test');
+};
 
 exports.login = function(req,res){
         
-    if(req.user)return res.json({code:0,message:'已经登录了'})
+    if(req.user)return res.json({code:0,message:'已经登录了'});
     
     if(util.isNullOrEmity(req.body.email) ||
        util.isNullOrEmity(req.body.password)){
-           return res.json({code:1,message:'密码，邮箱均为必填项'})
+           return res.json({code:1,message:'密码，邮箱均为必填项'});
        }
     
      User.findOne({email:req.body.email},function(err,user){           
             if (!user) {
-                return res.json({code:1,message:'用户名不存在'})
+                return res.json({code:1,message:'用户名不存在'});
             }
             if (!user.authenticate(req.body.password)) {
-                return res.json({code:1,message:'密码错误'})
+                return res.json({code:1,message:'密码错误'});
             }
             
             req.login(user, function(err) {
-                if (err)return res.json({code:1,message:"登录错误"})
-                res.json({code:0,message:'登录成功'})
-            })
+                if (err)return res.json({code:1,message:"登录错误"});
+                res.json({code:0,message:'登录成功'});
+            });
             
-        })
+        });
     
-}
+};
 
 exports.logout = function(req,res){
     req.logout();
-    res.json({code:0,message:'退出成功'})
-}
+    res.json({code:0,message:'退出成功'});
+};
 
 exports.register = function(req,res){
     
     if(util.isNullOrEmity(req.body.username) ||
        util.isNullOrEmity(req.body.password) || 
        util.isNullOrEmity(req.body.email)){
-           return res.json({code:1,message:'用户名，密码，邮箱均为必填项'})
+           return res.json({code:1,message:'用户名，密码，邮箱均为必填项'});
        }
     
     if(!util.isEmail(req.body.email)){
-        return res.json({code:1,message:'邮箱格式不正确'})
+        return res.json({code:1,message:'邮箱格式不正确'});
     }
     
     User.findOne({'email':req.body.email}).exec(function(err,user){        
-        if(user)res.json({code:1,message:'邮箱已经被注册'})
+        if(user)res.json({code:1,message:'邮箱已经被注册'});
         else{
              User.findOne({'username':req.body.username}).exec(function(err,user){
-                if(user)res.json({code:1,message:'用户名已经被注册'})
+                if(user)res.json({code:1,message:'用户名已经被注册'});
                 else{
-                    var u = new User(req.body)
-                    u.provider = 'local'
+                    var u = new User(req.body);
+                    u.provider = 'local';
                     u.save(function(err){
-                        if (err)res.json({code:1,message:err.errors})
+                        if (err)res.json({code:1,message:err.errors});
                         else {
                             req.login(u, function(err) {
-                                if (err)return res.json({code:1,message:"error"})
-                                res.json({code:0,message:'注册成功，并且已登录'})
-                            })
+                                if (err)return res.json({code:1,message:"error"});
+                                res.json({code:0,message:'注册成功，并且已登录'});
+                            });
                         }
-                    })
+                    });
                 }
-            })
+            });
         }
-    })
-}
+    });
+};
 
 exports.modifyPwd = function(req,res){
-    var user = req.user
+    var user = req.user;
     
-    if(!user)return res.json({code:1,message:'未授权'})
+    if(!user)return res.json({code:1,message:'未授权'});
     
     if(util.isNullOrEmity(req.body.password) || util.isNullOrEmity(req.body.oldPwd))
-    return res.json({code:1,message:'必要信息缺失'})
+    return res.json({code:1,message:'必要信息缺失'});
     
-    if(!user.authenticate(req.body.oldPwd))return res.json({code:1,message:'原密码错误'})
+    if(!user.authenticate(req.body.oldPwd))return res.json({code:1,message:'原密码错误'});
     
     user.password = req.body.password;        
     user.save(function(err){
-        if(err)return res.json({code:1,message:'修改错误'+err.errors})
+        if(err)return res.json({code:1,message:'修改错误'+err.errors});
         else return res.json({code:0,message:'修改成功'});
-    })
-}
+    });
+};
 
 exports.uploadAvatar = function(req,res){
-    var user = req.user
+    var user = req.user;
     
-    if(!user)return res.json({code:1,message:'未授权'})
+    if(!user)return res.json({code:1,message:'未授权'});
     
     var fileUpload = new FileUpload('image');
     
@@ -115,10 +115,10 @@ exports.uploadAvatar = function(req,res){
                 if(oldAvatar!=="")fileUpload.delete(oldAvatar);
                 return;
             }
-        })
-    })      
+        });
+    });
     res.send('上传中.');   
-}
+};
 
 exports.avatar = function(req,res){    
     var fileUpload = new FileUpload('image');
@@ -132,7 +132,7 @@ exports.avatar = function(req,res){
             res.send(data);
         }    
     ); 
-}
+};
 
 
 exports.follow = function(req,res){
@@ -155,7 +155,7 @@ exports.follow = function(req,res){
             });
         });        
     });    
-}
+};
 
 exports.disfollow = function(req,res){
     var user = req.user;    
@@ -168,31 +168,31 @@ exports.disfollow = function(req,res){
         
         Follow.findOneAndRemove({follow:uo._id},function(err){
             return res.json({code:0,message:'取消关注关系'});
-        })
+        });
     });    
-}
+};
 
 exports.followCount = function(req,res){
     
     if(util.isNullOrEmity(req.params.uid))return res.json({code:1,message:'参数缺损'});
     
     Follow.count({user:req.params.uid},function(err,count){
-        if(err)return res.json({code:1,message:'数据库出错'})
-        else return res.json({code:0,message:count);
-    })
+        if(err)return res.json({code:1,message:'数据库出错'});
+        else return res.json({code:0,message:count});
+    });
     
-}
+};
 
 exports.followedCount = function(req,res){
     
     if(util.isNullOrEmity(req.params.uid))return res.json({code:1,message:'参数缺损'});
     
     Follow.count({follow:req.params.uid},function(err,count){
-        if(err)return res.json({code:1,message:'数据库出错'})
-        else return res.json({code:0,message:count);
-    })
+        if(err)return res.json({code:1,message:'数据库出错'});
+        else return res.json({code:0,message:count});
+    });
     
-}
+};
 
 exports.followlist = function(req,res){
    
@@ -212,7 +212,7 @@ exports.followlist = function(req,res){
         if (err) {return res.json({code:1,message:'数据库查询错误'})}
         else {return res.json(follows)}
     });
-}
+};
 
 exports.followedlist = function(req,res){
     
@@ -232,7 +232,7 @@ exports.followedlist = function(req,res){
         if (err) {return res.json({code:1,message:'数据库查询错误'})}
         else {return res.json(follows)}
     });
-}
+};
 
 
 exports.isfollow = function(req,res){
@@ -246,6 +246,6 @@ exports.isfollow = function(req,res){
         if(follow)return res.json({code:0,message:"存在关注"});
         else return res.json({code:1,message:"不存在关注"});
     });
-}
+};
 
 
--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , crypto = require('crypto')
-  , _ = require('underscore')
+  , _ = require('underscore');
   
 var UserSchema = new Schema({
     email: String
@@ -14,36 +14,36 @@ var UserSchema = new Schema({
   , avatar:String
   , gender:String
   ,createdAt:{type: Date, default: Date.now}
-})
+});
 
 // virtual attributes
 UserSchema
   .virtual('password')
   .set(function(password) {
-    this._password = password
-    this.salt = this.makeSalt()
-    this.hashed_password = this.encryptPassword(password)
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
   })
-  .get(function() { return this._password })
+  .get(function() { return this._password });
 
 // validations
 var validatePresenceOf = function (value) {
-  return value && value.length
-}
+  return value && value.length;
+};
 
 // methods
 UserSchema.method('authenticate', function(plainText) {
-  return this.encryptPassword(plainText) === this.hashed_password
-})
+  return this.encryptPassword(plainText) === this.hashed_password;
+});
 
 UserSchema.method('makeSalt', function() {
-  return Math.round((new Date().valueOf() * Math.random())) + ''
-})
+  return Math.round((new Date().valueOf() * Math.random())) + '';
+});
 
 UserSchema.method('encryptPassword', function(password) {
-  return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
-})
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+});
 
 //UserSchema.method('updateAvatar',function())
 
-mongoose.model('User', UserSchema)
+mongoose.model('User', UserSchema);
